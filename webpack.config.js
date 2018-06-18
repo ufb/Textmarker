69699ffd2d8e47ac318598b765115538e9befb64
ufb <m.leugers@gmx.net>
2018-06-18 23:49:14 +0200

    avoid uglifyJS

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ module.exports = {
       path: path.resolve(__dirname, 'extension'),
       filename: '[name].wp.js'
     },
-    mode: PROD ? 'production' : 'development',
+    // avoid UglifyJS (-> https://developer.mozilla.org/en-US/Add-ons/Source_Code_Submission#Requirements_for_the_source_package)
+    mode: 'development',//PROD ? 'production' : 'development',
     devtool: PROD ? false : 'inline-source-map',
     module: {
       rules: [{
